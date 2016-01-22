38e5fd7c15777f032a34cab595207d70b78f7cc6
Patrick Shushereba <pshushereba@gmail.com>
2016-01-21 20:53:19 -0800

    Setting up problem

---
+++ imageblur1.rb
@@ -0,0 +1,19 @@
+class Image
+
+def initialize
+
+end
+
+def output_image
+
+end
+
+end
+
+image = Image.new([
+  [0, 0, 0, 0],
+  [0, 1, 0, 0],
+  [0, 0, 0, 1],
+  [0, 0, 0, 0]
+])
+image.output_image
