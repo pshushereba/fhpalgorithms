ccde5108d4060a36b2c1e4a854740ecb55e94944
Patrick Shushereba <pshushereba@gmail.com>
2016-01-22 11:11:07 -0800

    Closer to solving Image Blur 1

--- imageblur1.rb
+++ imageblur1.rb
@@ -1,14 +1,17 @@
 class Image
 
-def initialize
+	def initialize(image)
+		@image = image
+	end
+	
+	def output_image
+		@image.each do |x|
+			x.join
+		end
+	end
 
 end
 
-def output_image
-
-end
-
-end
 
 image = Image.new([
   [0, 0, 0, 0],
@@ -16,4 +19,4 @@ image = Image.new([
   [0, 0, 0, 1],
   [0, 0, 0, 0]
 ])
-image.output_image
+puts image.output_image
