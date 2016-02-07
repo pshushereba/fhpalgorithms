1f0b3bd17bd9f3780dec2b854dc941dc3bdbdeda
Patrick Shushereba <pshushereba@gmail.com>
2016-02-06 20:51:50 -0800

    Working on implementing blur method.

--- imageblur2.rb
+++ imageblur2.rb
@@ -6,14 +6,15 @@ class Image
 	
 	def output_image
 		@image.each { |row| puts row.join }
-		transform.each { |row_index, col_index| blur(row_index, col_index) }
+		#puts blur(row_index, col_index)
+		#transform.each { |row_index, col_index| blur(row_index, col_index) }
 	end 
 	
 	def blur(row_index,col_index)
-		@image[row_index-1][col_index] = 1
-		@image[row_index][col_index-1] = 1
-		@image[row_index][col_index+1] = 1
-		@image[row_index+1][col_index] = 1
+		@image[x-1][y] = 1
+		@image[x][y-1] = 1
+		@image[x][y+1] = 1
+		@image[x+1][y] = 1
 	end
 
 #First "1" in @image [1][1]
@@ -34,7 +35,7 @@ class Image
   			end
   		end
   	end
-	print one_index
+	#print one_index
     return Image.new(@image)
   end
 
